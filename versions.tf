a8a3c1021626b2276059f3633002c0e21d03c8c4
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2022-12-09 21:51:05 +0100

    adds providers

---
+++ versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+}
