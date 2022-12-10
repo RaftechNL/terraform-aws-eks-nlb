26d1e4fd8ce71ff392db62dcd78f7480456e521a
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2022-12-10 13:35:04 +0100

    adds descrition to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,6 @@
 output "cluster" {
   value = module.eks
+  description = "Contains all outputs from the EKS modules"
 }
 
 output "node_group" {
@@ -10,6 +11,8 @@ output "node_group" {
     "sys"      = module.sys_eks_managed_node_group
 
   }
+
+  description = "Contains all outputs from creating of the node groups"
 }
 
 output "nlb" {
@@ -17,5 +20,7 @@ output "nlb" {
     "external" = module.nlb_external
     "internal" = module.nlb_internal
   }
+
+  description = "Contains all outputs from creating internal/external loadbalancers"
 }
 
